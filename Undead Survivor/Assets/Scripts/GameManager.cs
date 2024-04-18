7f510e00e1b0ad199efb9fe472c94e43ee0f0e3f
mhyjw98 <rhapsodymis4@naver.com>
2024-04-18 22:06:52 +0900

    Level Up, 무기 구현

--- Undead Survivor/Assets/Scripts/GameManager.cs
+++ Undead Survivor/Assets/Scripts/GameManager.cs
@@ -5,24 +5,68 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
+    [Header("# Game Control")]
+    public bool isLive;
     public float gameTime;
     public float maxGameTime = 2 * 10f;
-
+    [Header("# Player Info")]
+    public int health;
+    public int maxHealth = 100;
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
+    [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
+    public LevelUp uiLevelUp;
 
     private void Awake()
     {
         instance = this;
     }
 
+    private void Start()
+    {
+        health = maxHealth;
+
+        // 임시 스크립트
+        uiLevelUp.Select(0);
+    }
+
     private void Update()
     {
+        if (!isLive)
+            return;
+
         gameTime += Time.deltaTime;
         if (gameTime > maxGameTime)
         {
             gameTime = maxGameTime;
         }
+    }
+
+    public void GetExp()
+    {
+        exp++;
+
+        if(exp == nextExp[Mathf.Min(level, nextExp.Length-1)])
+        {
+            level++;
+            exp = 0;
+            uiLevelUp.Show();
+        }
+    }
+
+    public void Stop()
+    {
+        isLive = false;
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        isLive = true;
+        Time.timeScale = 1;
     }
 }
