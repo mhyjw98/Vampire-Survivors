2e7ce208fb43784c0d773c04a60c6ed31dd4027a
mhyjw98 <rhapsodymis4@naver.com>
2024-04-20 00:08:39 +0900

    효과음, 배경음악 구현

--- Undead Survivor/Assets/Scripts/LevelUp.cs
+++ Undead Survivor/Assets/Scripts/LevelUp.cs
@@ -17,12 +17,16 @@ public class LevelUp : MonoBehaviour
         Next();
         rect.localScale = Vector3.one;
         GameManager.instance.Stop();
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.LevelUp);
+        AudioManager.instance.EffectBgm(true);
     }
 
     public void Hide()
     {
         rect.localScale = Vector3.zero;
         GameManager.instance.Resume();
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.Select);
+        AudioManager.instance.EffectBgm(false);
     }
 
     public void Select(int index)
@@ -32,6 +36,12 @@ public class LevelUp : MonoBehaviour
 
     void Next()
     {
+        bool isMaxHealth;
+        if(GameManager.instance.health < 100)
+            isMaxHealth = false;
+        else
+            isMaxHealth = true;
+
         // 모든 아이템 비활성화
         foreach (Item item in items)
         {
@@ -40,11 +50,12 @@ public class LevelUp : MonoBehaviour
 
         // 랜덤 3개의 아이템 활성화
         int[] ran = new int[3];
+
         while (true)
-        {
-            ran[0] = Random.Range(0, items.Length);
-            ran[1] = Random.Range(0, items.Length);
-            ran[2] = Random.Range(0, items.Length);
+        {                
+            ran[0] = Random.Range(0, isMaxHealth ? items.Length - 1 : items.Length);
+            ran[1] = Random.Range(0, isMaxHealth ? items.Length - 1 : items.Length);
+            ran[2] = Random.Range(0, isMaxHealth ? items.Length - 1 : items.Length);
 
             if (ran[0] != ran[1] && ran[0] != ran[2] && ran[1] != ran[2])
                 break;
@@ -56,9 +67,7 @@ public class LevelUp : MonoBehaviour
 
             // 아이템이 만렙일 경우 소비 아이템으로 대체
             if(ranItem.level == ranItem.data.damages.Length)
-            {
                 items[4].gameObject.SetActive(true);
-            }
             else
                 ranItem.gameObject.SetActive(true);
         }
